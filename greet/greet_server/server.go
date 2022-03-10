bd52eac54f292035005b0eb1384f7e5de6b387a1
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-03-10 11:05:42 +0700

    feat(Server Setup Boilerplate Code): Add commen server

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -13,14 +13,19 @@ type server struct{}
 func main() {
 	fmt.Println("Hello World")
 
+	// listen on port 50051
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+
+	// check error
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 
+	// create a new gRPC server
 	s := grpc.NewServer()
 	greetpb.RegisterGreetServiceServer(s, &server{})
 
+	//display eror message
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
