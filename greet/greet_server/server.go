ba26815562a705c45d019fc24442dd3de9762560
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-03-14 09:59:24 +0700

    feat(gRPC Unary): Add UNary API Server Implementation server

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/lopinhbest/GolangGRPC/greet/greetpb"
 	"google.golang.org/grpc"
@@ -9,6 +10,15 @@ import (
 )
 
 type server struct{}
+
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	firstName := req.GetGreeting().GetFirstName()
+	result := "Hello " + firstName
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
 
 func main() {
 	fmt.Println("Hello World")
