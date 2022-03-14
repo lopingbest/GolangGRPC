75d2fde24724a38c49164c21af455bed70c0fbf6
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-03-14 10:10:52 +0700

    feat(gRPC Unary): Add Unary API Client Implementation client

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/lopinhbest/GolangGRPC/greet/greetpb"
 	"google.golang.org/grpc"
@@ -16,8 +17,21 @@ func main() {
 	defer conn.Close()
 
 	c := greetpb.NewGreetServiceClient(conn)
-	fmt.Printf("Created client: %f", c)
-
+	//fmt.Printf("Created client: %f", c)
+	doUnary(c)
 }
 
-
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Loping",
+			LastName:  "Best",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from Greet: %v", res.Result)
+}
