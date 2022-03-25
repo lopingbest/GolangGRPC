81141459f7de3a155aa3e99fc79b98cda87663d3
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-03-25 22:34:54 +0700

    feat(ComputeAverage API): Add server

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/lopinhbest/GolangGRPC/calculator/calculatorpb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"io"
 	"log"
 	"net"
 )
@@ -41,6 +42,28 @@ func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositi
 		}
 	}
 	return nil
+}
+
+func (*server) ComputeAverage(stream calculatorpb.CalculatorService_ComputeAverageServer) error {
+	fmt.Printf("Received ComputeAverage RPC\n")
+
+	sum := int32(0)
+	count := 0
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			average := float64(sum) / float64(count)
+			return stream.SendAndClose(&calculatorpb.ComputeAverageResponse{
+				Average: average,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v", err)
+		}
+		sum += req.GetNumber()
+		count++
+	}
 }
 
 func main() {
