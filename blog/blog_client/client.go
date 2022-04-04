1c7fb31daac927a60cd944d3c6f44a2fe842d242
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2022-04-04 14:02:58 +0700

    feat(CRUD API with MongoDB): Add client

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -1,1 +1,37 @@
-package blog_client
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/lopinhbest/GolangGRPC/blog/blogpb"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main() {
+
+	fmt.Println("Blog Client")
+
+	opts := grpc.WithInsecure()
+
+	cc, err := grpc.Dial("localhost:50051", opts)
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := blogpb.NewBlogServiceClient(cc)
+
+	// create Blog
+	fmt.Println("Creating the blog")
+	blog := &blogpb.Blog{
+		AuthorId: "Stephane",
+		Title:    "My First Blog",
+		Content:  "Content of the first blog",
+	}
+	createBLogRes, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	if err != nil {
+		log.Fatalf("Unexpected error: %v", err)
+	}
+	fmt.Println("Blog has been created: %v", createBLogRes)
+}
